e88319a49e28a31ad53f6ae18756fb070c6cc1a3
callor <callor@callor.com>
2017-07-18 12:20:24 +0900

    2017-07-18

---
+++ Hello/Hello.ino
@@ -0,0 +1,21 @@
+/**
+* LED 1초 간격으로 깜빡거리기
+*
+*/
+// 시작하면서 해야할 일들
+void setup() {
+
+  // 디지털 핀 (0-13번까지)을 어떤 용도로 쓸것인가를 설정
+  pinMode(5,OUTPUT); // 신호를 ar-> 외부로 보내겠다고 설정
+  pinMode(4,OUTPUT); // INPUT : 외부의 신호값을 읽어 들이겠다. 설정
+
+}
+void loop() {
+
+  // 신호를 5번핀을 통해서 내보는 명령
+  digitalWrite(5,HIGH); // HIGH : 이진수의 1의 값을 Pin 5번에 보내라
+  delay(1000); // 잠시 멈추어라, 1초동안 현상태 유지
+  digitalWrite(5,LOW);
+  delay(1000);
+
+}
---
+++ Hello_Sw_01/Hello_Sw_01.ino
@@ -0,0 +1,43 @@
+/**
+ * 스위치 입력에 따라 LED 깜빡거리기
+ */
+void setup() {
+  // 5번(D5) 핀을 출력모드로 설정
+  pinMode(5,OUTPUT);
+  pinMode(4,OUTPUT);
+  
+  // 스위치를 연결하는 포트는 INPUT으로 사용하지 않고, INPUT_PULLUP으로 사용한다.
+  pinMode(3,INPUT_PULLUP);
+
+}
+
+void loop() {
+
+  // 3번핀에 HIGH값이 입력되면, 스위치가 OFF 인상태
+  if(digitalRead(3) == HIGH){
+    // 5번핀에 LOW 보내서 LED 를 끄고
+    digitalWrite(5,LOW);
+
+    // 그렇지 않으면, 스위치가 ON 인 상태
+  } else {
+
+    // 5번핀에 HIGH, LOW를 교대로 보내서 LED 깜빡거리기
+    digitalWrite(5,HIGH);
+    delay(1000);
+    digitalWrite(5,LOW);
+    delay(1000);
+    
+  }
+
+
+
+//  // 5번핀에 HIGH (5V)를 내보내고
+//  digitalWrite(5,HIGH);
+//  // 1초가 유지
+//  delay(1000);
+//
+//  // 5번핀에 LOW(0V)를 내보내고
+//  digitalWrite(5,LOW);
+//  delay(1000); // 1초간 유지
+
+}
